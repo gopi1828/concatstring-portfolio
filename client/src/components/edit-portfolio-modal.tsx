2279b031d661bf701ed7c7204eb919b47a7bd3e0
Vishvam253 <vishvamdhanani5@gmail.com>
2025-09-24 16:26:33 +0530

    Update error edit-portfolio-modal.tsx

--- client/src/components/edit-portfolio-modal.tsx
+++ client/src/components/edit-portfolio-modal.tsx
@@ -8,7 +8,7 @@ import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
 import { Textarea } from "../components/ui/textarea";
-import { Badge } from "../components/ui/badge";
+
 import {
   Dialog,
   DialogContent,
@@ -17,7 +17,7 @@ import {
   DialogHeader,
   DialogTitle,
 } from "../components/ui/dialog";
-import { Upload, X, Plus } from "lucide-react";
+import { Upload, X } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
 interface EditPortfolioModalProps {
